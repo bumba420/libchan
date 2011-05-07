77da9330307bab29f3112f5e3d3bab83dd79e5b1
benpicco <benpicco@googlemail.com>
2011-05-07 12:10:19 +0200

    add timestamp to log messages

--- src/de/benpicco/clichan/StdLogger.java
+++ src/de/benpicco/clichan/StdLogger.java
@@ -1,11 +1,20 @@
 package de.benpicco.clichan;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import de.benpicco.libchan.util.LoggerBackend;
 
 /**
  * Writes log messages to stdout/stderr.
  */
 public class StdLogger implements LoggerBackend {
+	private final SimpleDateFormat	format	= new SimpleDateFormat("[HH:mm:ss] ");
+
+	private String time() {
+		return format.format(Calendar.getInstance().getTime());
+	}
+
 	@Override
 	public void print(String msg) {
 		System.out.print(msg);
@@ -13,11 +22,11 @@ public class StdLogger implements LoggerBackend {
 
 	@Override
 	public void error(String msg) {
-		System.err.println(msg);
+		System.err.println(time() + msg);
 	}
 
 	@Override
 	public void println(String msg) {
-		System.out.println(msg);
+		System.out.println(time() + msg);
 	}
 }
