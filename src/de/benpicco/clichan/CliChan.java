fe7d52ebc56162054bd0a8f91de4e57fe70e15ad
benpicco <benpicco@googlemail.com>
2011-05-09 20:29:11 +0200

    buxfix release

--- src/de/benpicco/clichan/CliChan.java
+++ src/de/benpicco/clichan/CliChan.java
@@ -19,7 +19,7 @@ import de.benpicco.libchan.util.FileUtil;
 import de.benpicco.libchan.util.Logger;
 
 public class CliChan {
-	public final static String	VERSION	= "0.2";
+	public final static String	VERSION	= "0.2.1";
 
 	public static void main(String[] args) {
 		String[] urls = null;
