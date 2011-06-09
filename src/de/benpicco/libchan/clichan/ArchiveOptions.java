d7d249905f2c9b8cfe177e343fcb20abc68c89a4
benpicco <benpicco@googlemail.com>
2011-06-10 00:29:34 +0200

    start working on deleted post detection

--- src/de/benpicco/libchan/clichan/ArchiveOptions.java
+++ src/de/benpicco/libchan/clichan/ArchiveOptions.java
@@ -3,13 +3,52 @@ package de.benpicco.libchan.clichan;
 import java.util.List;
 
 public class ArchiveOptions {
+	/**
+	 * refresh interval in s, no refresh when <= 0
+	 */
 	public int			interval;
+
+	/**
+	 * save images
+	 */
 	public boolean		archiveThread;
+
+	/**
+	 * where to save all the stuff
+	 */
 	public String		target;
+
+	/**
+	 * use separate folders for threads
+	 */
 	public boolean		threadFolders;
+
+	/**
+	 * save thread as html
+	 */
 	public boolean		saveHtml;
+
+	/**
+	 * names send a notification on
+	 */
 	public List<String>	names;
+
+	/**
+	 * whether to record posting stats or not
+	 */
 	public boolean		recordStats;
+	/**
+	 * the FollowUpTag, or null if archiving should not follow new threads
+	 */
 	public String		followUpTag;
+
+	/**
+	 * chan config dir
+	 */
 	public String		config;
+
+	/**
+	 * watch for deleted posts and delete the image
+	 */
+	public boolean		delete;
 }
