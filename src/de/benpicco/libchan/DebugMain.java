43cc8143ff632476c012bdf24f18e2dec66487f5
benpicco <benpicco@googlemail.com>
2011-07-01 22:41:47 +0200

    fix some issue with kusabax

--- src/de/benpicco/libchan/DebugMain.java
+++ src/de/benpicco/libchan/DebugMain.java
@@ -37,7 +37,7 @@ public class DebugMain {
 	public static void main(final String[] args) throws MalformedURLException, IOException, InterruptedException {
 		Logger.add(new StdLogger());
 
-		String url = "http://boards.4chan.org/sci/res/3316575";
+		String url = "http://7chan.org/777/res/448.html";
 		// String url = "http://www.0chan.ru/e/";
 		// String url = "http://operatorchan.org/k/";
 
