f4b7ee81bb313be6264fd5a1c565621c04670037
benpicco <benpicco@googlemail.com>
2011-07-11 01:05:34 +0200

    limit parallel downloads fixes issue 5

---
+++ src/de/benpicco/libchan/util/ThreadPool.java
@@ -0,0 +1,46 @@
+package de.benpicco.libchan.util;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class ThreadPool {
+	private static int									threadCount	= 0;
+	private static int									poolSize	= 20;
+	private static BlockingQueue<Tuple<String, String>>	queue		= new LinkedBlockingDeque<Tuple<String, String>>();
+
+	public static void addDownload(String url, String dest) {
+		queue.add(new Tuple<String, String>(url, dest));
+		governThread(1);
+	}
+
+	public static void setMaxThread(int threads) {
+		poolSize = threads;
+	}
+
+	protected static synchronized void governThread(int i) {
+		if (i > 0)
+			for (int j = 0; j <= i && threadCount <= poolSize; ++j, ++threadCount)
+				new workerThread(queue).start();
+		else
+			threadCount -= i;
+		if (threadCount < 0)
+			threadCount = 0;
+	}
+}
+
+class workerThread extends Thread {
+	private final BlockingQueue<Tuple<String, String>>	queue;
+
+	public workerThread(BlockingQueue<Tuple<String, String>> queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		Tuple<String, String> job;
+		while ((job = queue.poll()) != null)
+			FileUtil.downloadFile(job.first, job.second, 5);
+		ThreadPool.governThread(-1);
+	}
+
+}
