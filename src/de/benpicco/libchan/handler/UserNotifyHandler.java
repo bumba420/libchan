0204d4299e978919365e8c1c6ac3ea1d8ea0d2c7
benpicco <benpicco@googlemail.com>
2011-07-19 21:16:46 +0200

    make notify option consistent in that we only need to specify a substring that has to match

--- src/de/benpicco/libchan/handler/UserNotifyHandler.java
+++ src/de/benpicco/libchan/handler/UserNotifyHandler.java
@@ -13,52 +13,58 @@ import de.benpicco.libchan.util.Logger;
 
 public class UserNotifyHandler implements PostProcessor {
 
-	private final List<String>	names;
+	private final String[]	names;
 
 	public UserNotifyHandler(List<String> names) {
-		this.names = names;
+		this.names = new String[names.size()];
+		for (int i = 0; i < this.names.length; ++i)
+			this.names[i] = names.get(i).toLowerCase();
 	}
 
 	@Override
 	public void onAddPost(Post post) {
-		if (names.contains(post.user.toLowerCase())) {
-			Logger.get().println("New post from " + post.user);
+		for (int i = 0; i < names.length; ++i)
+			if (post.user.toLowerCase().contains(names[i])) {
+				Logger.get().println("New post from " + post.user);
 
-			String tempfile = "";
-			if (post.images.size() > 0) {
-				String url = post.images.get(0).thumbnailUrl;
-				try {
-					tempfile = FileUtil.prepareDir(System.getProperty("java.io.tmpdir") + File.separator + "libChan")
-							+ StringUtils.substringAfterLast(url, "/");
-					FileUtil.downloadFile(url, tempfile);
-				} catch (IOException e) {
-					Logger.get().error("Failed fetching thumbnail: " + e);
+				String tempfile = "";
+				if (post.images.size() > 0) {
+					String url = post.images.get(0).thumbnailUrl;
+					try {
+						tempfile = FileUtil.prepareDir(System.getProperty("java.io.tmpdir") + File.separator
+								+ "libChan")
+								+ StringUtils.substringAfterLast(url, "/");
+						FileUtil.downloadFile(url, tempfile);
+					} catch (IOException e) {
+						Logger.get().error("Failed fetching thumbnail: " + e);
+					}
 				}
-			}
 
-			String[] cmd = null;
-			String[] cmd_linux = { "notify-send", "-i", tempfile, post.user, post.message };
-			String[] cmd_osx = { "growlnotify", "--image", tempfile, "-t", post.user, "-m", post.message };
-			String[] cmd_win = { "growlnotify", "/i:", tempfile, "/t:", post.user, post.message };
+				String[] cmd = null;
+				String[] cmd_linux = { "notify-send", "-i", tempfile, post.user, post.message };
+				String[] cmd_osx = { "growlnotify", "--image", tempfile, "-t", post.user, "-m", post.message };
+				String[] cmd_win = { "growlnotify", "/i:", tempfile, "/t:", post.user, post.message };
 
-			String uname = System.getProperty("os.name").toLowerCase();
-			if (uname.contains("windows"))
-				cmd = cmd_win;
-			else if (uname.contains("linux"))
-				cmd = cmd_linux;
-			else if (uname.contains("mac"))
-				cmd = cmd_osx;
-			else {
-				Logger.get().println(uname + " notification not supported");
-				return;
-			}
+				String uname = System.getProperty("os.name").toLowerCase();
+				if (uname.contains("windows"))
+					cmd = cmd_win;
+				else if (uname.contains("linux"))
+					cmd = cmd_linux;
+				else if (uname.contains("mac"))
+					cmd = cmd_osx;
+				else {
+					Logger.get().println(uname + " notification not supported");
+					return;
+				}
 
-			try {
-				Runtime.getRuntime().exec(cmd);
-			} catch (IOException e) {
-				Logger.get().error("can't display notification");
+				try {
+					Runtime.getRuntime().exec(cmd);
+				} catch (IOException e) {
+					Logger.get().error("can't display notification");
+				}
+
+				break;
 			}
-		}
 	}
 
 	@Override
