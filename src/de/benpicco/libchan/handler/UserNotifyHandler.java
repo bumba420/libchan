eff3804ad8d77276fbbf050753237d8455f5f0d3
benpicco <benpicco@googlemail.com>
2011-07-26 19:23:15 +0200

    use common function to find element in array

--- src/de/benpicco/libchan/handler/UserNotifyHandler.java
+++ src/de/benpicco/libchan/handler/UserNotifyHandler.java
@@ -10,6 +10,7 @@ import de.benpicco.libchan.imageboards.Post;
 import de.benpicco.libchan.interfaces.PostProcessor;
 import de.benpicco.libchan.util.FileUtil;
 import de.benpicco.libchan.util.Logger;
+import de.benpicco.libchan.util.Misc;
 
 public class UserNotifyHandler implements PostProcessor {
 
@@ -23,48 +24,44 @@ public class UserNotifyHandler implements PostProcessor {
 
 	@Override
 	public void onAddPost(Post post) {
-		for (int i = 0; i < names.length; ++i)
-			if (post.user.toLowerCase().contains(names[i])) {
-				Logger.get().println("New post from " + post.user);
+		if (Misc.containsAlike(names, post.user.toLowerCase()) >= 0) {
+			Logger.get().println("New post from " + post.user);
 
-				String tempfile = "";
-				if (post.images.size() > 0) {
-					String url = post.images.get(0).thumbnailUrl;
-					try {
-						tempfile = FileUtil.prepareDir(System.getProperty("java.io.tmpdir") + File.separator
-								+ "libChan")
-								+ StringUtils.substringAfterLast(url, "/");
-						FileUtil.downloadFile(url, tempfile);
-					} catch (IOException e) {
-						Logger.get().error("Failed fetching thumbnail: " + e);
-					}
-				}
-
-				String[] cmd = null;
-				String[] cmd_linux = { "notify-send", "-i", tempfile, post.user, post.message };
-				String[] cmd_osx = { "growlnotify", "--image", tempfile, "-t", post.user, "-m", post.message };
-				String[] cmd_win = { "growlnotify", "/i:", tempfile, "/t:", post.user, post.message };
-
-				String uname = System.getProperty("os.name").toLowerCase();
-				if (uname.contains("windows"))
-					cmd = cmd_win;
-				else if (uname.contains("linux"))
-					cmd = cmd_linux;
-				else if (uname.contains("mac"))
-					cmd = cmd_osx;
-				else {
-					Logger.get().println(uname + " notification not supported");
-					return;
-				}
-
+			String tempfile = "";
+			if (post.images.size() > 0) {
+				String url = post.images.get(0).thumbnailUrl;
 				try {
-					Runtime.getRuntime().exec(cmd);
+					tempfile = FileUtil.prepareDir(System.getProperty("java.io.tmpdir") + File.separator + "libChan")
+							+ StringUtils.substringAfterLast(url, "/");
+					FileUtil.downloadFile(url, tempfile);
 				} catch (IOException e) {
-					Logger.get().error("can't display notification");
+					Logger.get().error("Failed fetching thumbnail: " + e);
 				}
-
-				break;
 			}
+
+			String[] cmd = null;
+			String[] cmd_linux = { "notify-send", "-i", tempfile, post.user, post.message };
+			String[] cmd_osx = { "growlnotify", "--image", tempfile, "-t", post.user, "-m", post.message };
+			String[] cmd_win = { "growlnotify", "/i:", tempfile, "/t:", post.user, post.message };
+
+			String uname = System.getProperty("os.name").toLowerCase();
+			if (uname.contains("windows"))
+				cmd = cmd_win;
+			else if (uname.contains("linux"))
+				cmd = cmd_linux;
+			else if (uname.contains("mac"))
+				cmd = cmd_osx;
+			else {
+				Logger.get().println(uname + " notification not supported");
+				return;
+			}
+
+			try {
+				Runtime.getRuntime().exec(cmd);
+			} catch (IOException e) {
+				Logger.get().error("can't display notification");
+			}
+		}
 	}
 
 	@Override
